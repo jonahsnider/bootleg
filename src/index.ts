089e204838bbfc01de5f863b21ac01b75b5f1a89
Jonah Snider <jonah@jonah.pw>
2021-01-19 02:50:22 -0800

    fix: add concurrency limits to downloads

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import {Command, flags} from '@oclif/command';
 import {InstagramDownloader} from './downloaders';
 import {readLines} from './file-parser';
+import PromiseQueue from 'p-queue';
+import cli from 'cli-ux';
 
-const allDownloaders = [new InstagramDownloader()];
+const allDownloaders = [new InstagramDownloader()] as const;
 
 class Bootleg extends Command {
 	static flags = {
@@ -18,22 +20,39 @@ class Bootleg extends Command {
 		})
 	};
 
+	private readonly downloadQueue = new PromiseQueue({concurrency: 1, autoStart: false});
+	private readonly progress = cli.progress();
+
 	async run() {
 		const {flags} = this.parse(Bootleg);
 
 		const urls = await readLines(flags.file);
 
-		urls.forEach(async url => {
+		for (const url of urls) {
 			const downloader = allDownloaders.find(downloader => downloader.canDownload(url));
 
 			if (!downloader) {
 				throw new RangeError(`Couldn't find a downloader for ${url}`);
 			}
 
+			// eslint-disable-next-line no-await-in-loop
 			const ids = await downloader.resolveIds(url);
 
-			ids.forEach(async id => downloader.download({directory: flags.dir, media: id}));
-		});
+			const downloads = ids.map(id => async () => {
+				await downloader.download({directory: flags.dir, media: id});
+				this.progress.increment();
+			});
+
+			// eslint-disable-next-line no-await-in-loop
+			await this.downloadQueue.addAll(downloads);
+		}
+
+		this.progress.start(this.downloadQueue.size);
+		this.downloadQueue.start();
+
+		await this.downloadQueue.onIdle();
+
+		this.progress.stop();
 	}
 }
 // eslint-disable-next-line import/no-extraneous-dependencies, @typescript-eslint/no-var-requires
